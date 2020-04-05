70ad90b4799959c08370721836a47995db3c35b4
Tsolovikos Theologos <teotsolos@hotmail.gr>
2020-04-05 16:15:33 +0300

    Finish Responsive Design
    * Added Responsive design on all sections from mobile top desktop
    + Added overlay menu on mobile/tablet
    + Show/hide go to top btn  after some scroll down

--- src/app/navigation.js
+++ src/app/navigation.js
@@ -1,4 +1,39 @@
 //add on scroll bg on nav
 //add on scroll each section active link
 //add on scroll header transparent or not 
-//add toggle overlay display menu
+
+//Add/Remove Overlay Menu on Max screen width 800px on click 
+  const menuNav=document.querySelector(".nav_menu");
+  const menuHam=document.querySelector("#menuHam");
+  const menuNavBar=document.querySelector(".navbar__nav");
+  const navlinks=document.querySelectorAll(".nav__link");
+
+  menuHam.addEventListener("click",(e)=>{
+    e.preventDefault();
+    menuNav.classList.toggle("open");
+    menuHam.classList.toggle("open");
+    menuNavBar.classList.toggle("open");
+    navlinks.forEach(link=>{
+      link.addEventListener('click',()=>{
+        menuNav.classList.remove("open");
+        menuHam.classList.remove("open");
+        menuNavBar.classList.remove("open");
+      });
+   });
+  });
+// End of show/hide overlay menu
+
+//Show/hide Go to top btn if use scroll down 100px
+const btnGoToTop=document.querySelector("#btnGoTop");
+
+window.addEventListener('scroll',()=>{
+  if(window.pageYOffset > 300)
+  {
+    btnGoToTop.style.display="block";
+  }
+  else{
+    btnGoToTop.style.display="none";
+  }
+});
+
+/* end  Back to Top button  */
