11d266baa626322760c3b6aab858d68b88c46592
Tsolovikos Theologos <teotsolos@hotmail.gr>
2020-04-08 17:55:26 +0300

    Some pkg update & fixed run prod bug

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,11 @@
 const path = require("path");
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-let HtmlWebpackPlugin=require('html-webpack-plugin');
 module.exports = {
   entry: {
     main:"./src/index.js",
     vendor:"./src/vendor.js"
   },
-  plugins: [
-    new FaviconsWebpackPlugin({
-      logo: './src/assets/imgs/logo.png',
-      mode: 'webapp', 
-      devMode: 'webapp', 
-      favicons: {
-        appName: 'photographer_portfolio',
-        appDescription: 'Study Case Photographers Portfolio',
-        developerName: 'Theologos Tsolovikos',
-        developerURL: null, 
-        background: '#fff',
-        theme_color: '#1A242D',
-        icons: {
-          coast: false,
-          yandex: false
-        }
-      }})
-  ],
+ 
   module: {
     rules: [
         {
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,8 @@ const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -29,7 +30,23 @@ module.exports = merge(common, {
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: './src/assets/imgs/logo.png',
+      mode: 'webapp', 
+      devMode: 'webapp', 
+      favicons: {
+        appName: 'photographer_portfolio',
+        appDescription: 'Study Case Photographers Portfolio',
+        developerName: 'Theologos Tsolovikos',
+        developerURL: null, 
+        background: '#fff',
+        theme_color: '#1A242D',
+        icons: {
+          coast: false,
+          yandex: false
+        }
+      }})
   ],
   module: {
     rules: [
